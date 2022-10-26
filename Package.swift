02baeae62298cf489b28a64a19809ee09377d4b5
Kyle Hammond <kyle.hammond@jamf.com>
2022-10-26 11:20:13 -0500

    Add workflows for running unit tests and publishing documentation from GitHub Actions.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
@@ -10,6 +10,9 @@ let package = Package(
     ],
     products: [
         .library(name: "ManagedAppConfigLib", targets: ["ManagedAppConfigLib"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
     ],
     targets: [
         .target(name: "ManagedAppConfigLib"),
